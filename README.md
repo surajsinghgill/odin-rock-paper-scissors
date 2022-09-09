9699aa43148fde484d2a23e91674103309b52fb3
surajsinghgill <surajsinghgill123@gmail.com>
2022-09-09 15:23:44 +0530

    Add simple User Interface

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # odin-rock-paper-scissors
+
+#hand rock
+<a href="https://www.flaticon.com/free-icons/fist" title="fist icons">Fist icons created by Cuputo - Flaticon</a>
+
+#hand scissor
+<a href="https://www.flaticon.com/free-icons/scissor" title="scissor icons">Scissor icons created by juicy_fish - Flaticon</a>
+
+#hand paper
+<a href="https://www.flaticon.com/free-icons/hand" title="hand icons">Hand icons created by berkahicon - Flaticon</a>
+
+#question mark
+<a href="https://www.flaticon.com/free-icons/question" title="question icons">Question icons created by Freepik - Flaticon</a>
--- game.js
+++ game.js
@@ -2,6 +2,7 @@ function getComputerChoice() {
     const array = ["rock", "paper", "scissors"];
     return array[Math.floor(Math.random() * 3)];
 }
+
 function playRound(playerSelection, computerSelection) {
     switch(true) {
         case (playerSelection === "rock" && computerSelection === "paper"):
@@ -21,29 +22,94 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Enter your choice").toLowerCase();
-        let result = playRound(playerChoice, getComputerChoice());
-        console.log(result);
-        if (result.startsWith("You win")) {
-            scorePlayer++;
-        }
-        else if (result.startsWith("You lose")) {
-            scoreComputer++;
-        }
-    }
-    if (scorePlayer > scoreComputer) {
-        console.log("You win");
-    }
-    else if (scorePlayer === scoreComputer) {
-        console.log("Draw");
-    }
-    else {
-        console.log("You lose");
+function playerImage(playerSelection) {
+    const imagePlayer = document.querySelector("#player");
+    switch(playerSelection) {
+        case "rock":
+            imagePlayer.setAttribute("src", "./images/fist.png");
+            imagePlayer.setAttribute("alt", "rock");
+            break;
+        case "paper":
+            imagePlayer.setAttribute("src", "./images/hand.png");
+            imagePlayer.setAttribute("alt", "paper");
+            break;
+        case "scissors":
+            imagePlayer.setAttribute("src", "./images/scissor.png");
+            imagePlayer.setAttribute("alt", "scissors");
     }
 }
 
-game();
+function computerImage(computerSelection) {
+    const imageComputer = document.querySelector("#computer");
+    switch(computerSelection) {
+        case "rock":
+            imageComputer.setAttribute("src", "./images/fist.png");
+            imageComputer.setAttribute("alt", "rock");
+            break;
+        case "paper":
+            imageComputer.setAttribute("src", "./images/hand.png");
+            imageComputer.setAttribute("alt", "paper");
+            break;
+        case "scissors":
+            imageComputer.setAttribute("src", "./images/scissor.png");
+            imageComputer.setAttribute("alt", "scissors");
+    }
+}
+
+function reset() {
+    const imagePlayer = document.querySelector("#player");
+    const imageComputer = document.querySelector("#computer");
+    imagePlayer.setAttribute("src", "./images/question-mark.png");
+    imagePlayer.setAttribute("alt", "question mark");
+    imageComputer.setAttribute("src", "./images/question-mark.png");
+    imageComputer.setAttribute("alt", "question mark");
+    document.querySelectorAll(".score").forEach(score => score.textContent = "0");
+    document.querySelector("#flex-item-round p").textContent = "";
+}
+
+function updateScore(round){
+    const scores = document.querySelectorAll(".score");
+    let score1 = parseInt(scores[0].textContent);
+    let score2 = parseInt(scores[1].textContent);
+    if (round.startsWith("You win")) {
+        score1++;
+    }
+    else if (round.startsWith("You lose")) {
+        score2++;
+    }
+    scores[0].textContent = `${score1}`;
+    scores[1].textContent = `${score2}`;
+
+    if (score1 === 5) {
+        document.querySelector(".content").textContent = "You won!"
+        document.querySelector("#modal").style.display = "block";
+        reset();
+    }
+    else if (score2 === 5) {
+        document.querySelector(".content").textContent = "You lose!"
+        document.querySelector("#modal").style.display = "block";
+        reset();
+    }
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        let computerChoice = getComputerChoice();
+        let round = playRound(button.id, computerChoice);  
+        const roundResult = document.querySelector("#flex-item-round p");
+
+        roundResult.textContent = round;
+
+        playerImage(button.id);
+
+        computerImage(computerChoice);
+
+        updateScore(round);
+})
+});
+
+
+document.querySelector(".close").addEventListener("click", function() {
+    document.querySelector("#modal").style.display = "none";
+});
